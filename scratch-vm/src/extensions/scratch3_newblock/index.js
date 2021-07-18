7b9cd8465aad3c2ab4b3213a7313ced0616f2d0c
funny4875 <funny4875@gmail.com>
2021-07-19 06:50:12 +0800

    Update index.js

--- scratch-vm/src/extensions/scratch3_newblock/index.js
+++ scratch-vm/src/extensions/scratch3_newblock/index.js
@@ -5,61 +5,57 @@ const log = require('../../util/log');
 var global_temp = "";
 class newblock 
 {
-	get TEST_MENU () {
-      return [
-          {
-            text: '1',
-            value: '1'
-          },
-          {
-            text: '2',
-            value: '2'
-          }
-      ]
-    }
-    constructor (runtime) {this.runtime = runtime;}
-    getInfo () 
+	get TEST_MENU () 
 	{
-        return {
-            id: 'newblocks',
-            name: 'New Blocks',
-            blocks: [
-                {
-                    opcode: 'test',
-                    blockType: BlockType.COMMAND,
-                    text: 'Test [DATA][OPTION]',
-                    arguments: {
-						DATA: {
-                            type: ArgumentType.STRING,
-                            defaultValue: "data"
-                        },
-						OPTION: {
-                            type: ArgumentType.STRING,
-							menu: 'testMenu',
-                            defaultValue: ""
-                        }
-                    }
-                },
-				{
-                    opcode: 'getValue',
-                    blockType: BlockType.REPORTER,
-                    text: 'Value'
-                },
-            ],
-            menus: 
+		return [{text: '1',value: '1'},{text: '2',value: '2'}]
+	}
+	constructor (runtime) {this.runtime = runtime;}
+	getInfo()
+	{
+		return 
+		{
+			id: 'newblocks',
+			name: 'New Blocks',
+			blocks:[
 			{
-				testMenu: {
+				opcode: 'function1',
+				blockType: BlockType.REPORTER,
+				text: 'Value'
+			},
+			{
+				opcode: 'function2',
+				blockType: BlockType.COMMAND,
+				text: 'Test [DATA][OPTION]',
+				arguments: 
+				{
+					DATA: 
+					{
+						type: ArgumentType.STRING,defaultValue: "data"
+					},
+					OPTION:
+					{
+						type: ArgumentType.STRING,
+						menu: 'testMenu',
+						defaultValue: ""
+					}
+				}
+			},],
+			menus:
+			{
+				testMenu: 
+				{
 					acceptReporters: true,
 					items: this.TEST_MENU
 				}
 			}
-        };
-    };
-	getValue(){
+		};
+	};
+	
+	function1(){
 		return global_temp;
 	}
 	
-	test(args)
+	function2(args)
 	{
 		const DATA = Cast.toString(args.DATA);
 		const OPTION = Cast.toString(args.OPTION);
